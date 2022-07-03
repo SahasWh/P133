4aa44efff89f3ee2aa5c2c20ee406dcbee69f58b
SahasWh <82063178+SahasWh@users.noreply.github.com>
2022-07-03 11:11:43 -0400

    Add files via upload

---
+++ remote.js
@@ -0,0 +1,50 @@
+img = "";
+
+Status = "";
+
+objects = [];
+
+function preload() { 
+    img = loadImage("remote.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 350);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("modelLoaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+
+    objects = results;
+}
+
+function draw(){
+    image(img,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+
+            text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+}
